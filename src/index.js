c9790b07e1152ff82da5174da515b167c9ed6efa
Joel John <johnjoel079@gmail.com>
2024-04-04 12:56:19 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // your code here 
+ 
+const userInput = document.getElementById("new-task-description") 
+const form = document.getElementById("create-task-form") 
+const list = document.getElementById("tasks")  
+
+form.addEventListener(`submit`,(e)=> handleSubmit(e)) 
+
+function handleSubmit(e) { 
+e.preventDefault() 
+const li = document.createElement("li") 
+li.textContent = userInput.value 
+
+if (userInput.value !== ""){ 
+  list.appendChild(li)
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); 
